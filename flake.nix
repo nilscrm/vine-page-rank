1f6efcdb1fc320cc0e3876be8eeac33aa1ab996d
Nils Cremer <38761406+nilscrm@users.noreply.github.com>
2026-01-03 17:27:41 +0100

    add nix flake (#3)
    * add nix flake
    
    * use flake for ci
    
    * remove dprint

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Vine Page Rank";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    vine = {
+      url = "github:VineLang/vine";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      vine,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        vineCli = vine.packages.${system}.vine;
+      in
+      {
+        formatter = pkgs.nixfmt-tree;
+
+        checks.run-page-rank =
+          pkgs.runCommand "run-page-rank"
+            {
+              nativeBuildInputs = [ vineCli ];
+            }
+            ''
+              export HOME=$TMPDIR
+              vine run --no-stats ${./page_rank.vi}
+              mkdir -p $out
+            '';
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            vineCli
+          ];
+        };
+      }
+    );
+}
